007c1132e21782791d0691148022d2e900d9ccf5
Malachi Soord <inverse.chi@gmail.com>
2023-11-11 22:14:33 +0100

    Add another test case

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -3,7 +3,9 @@ from unittest import TestCase
 import pytest
 import vcr
 
-from cert_host_scraper.scraper import Options, fetch_urls
+from cert_host_scraper.scraper import Options, fetch_site_information, fetch_urls
+
+TIMEOUT = 2
 
 
 class TestScraper(TestCase):
@@ -13,3 +15,9 @@ class TestScraper(TestCase):
         results = fetch_urls("example.org", Options(timeout=2, clean=True))
         self.assertEqual(4, len(results))
         self.assertIn("https://www.example.org", results)
+
+    @pytest.mark.enable_socket
+    @vcr.use_cassette("fixtures/vcr/fetch_site_information_valid.yaml")
+    def test_fetch_site_information_valid(self):
+        result = fetch_site_information("https://example.org", TIMEOUT)
+        self.assertEqual(200, result)
