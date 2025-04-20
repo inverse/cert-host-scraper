3f2cb306e85506c0422ab5602d77a6e815cd66b5
Malachi Soord <inverse.chi@gmail.com>
2025-04-20 02:09:28 +0200

    Additional test (#173)

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import pytest
 import vcr
 
-from cert_host_scraper.scraper import Options, fetch_site_information, fetch_urls
+from cert_host_scraper import scraper
 
 TIMEOUT = 2
 
@@ -12,19 +12,38 @@ class TestScraper(TestCase):
     @pytest.mark.enable_socket
     @vcr.use_cassette("fixtures/vcr/google.com.yaml")
     def test_fetch_urls_clean_true(self):
-        results = fetch_urls("google.com", Options(timeout=2, clean=True))
+        results = scraper.fetch_urls(
+            "google.com", scraper.Options(timeout=2, clean=True)
+        )
         self.assertEqual(53, len(results))
         self.assertIn("https://www.google.com", results)
 
     @pytest.mark.enable_socket
     @vcr.use_cassette("fixtures/vcr/google.com.yaml")
     def test_fetch_urls_clean_false(self):
-        results = fetch_urls("google.com", Options(timeout=2, clean=False))
+        results = scraper.fetch_urls(
+            "google.com", scraper.Options(timeout=2, clean=False)
+        )
         self.assertEqual(60, len(results))
         self.assertIn("https://*.mail.google.com", results)
 
     @pytest.mark.enable_socket
     @vcr.use_cassette("fixtures/vcr/fetch_site_information_valid.yaml")
     def test_fetch_site_information_valid(self):
-        result = fetch_site_information("https://example.org", TIMEOUT)
+        result = scraper.fetch_site_information("https://example.org", TIMEOUT)
         self.assertEqual(200, result)
+
+
+class TestResults(TestCase):
+    def test_filter_by_status_code(self):
+        results = scraper.Result(
+            [
+                scraper.UrlResult("https://example-200.org", 200),
+                scraper.UrlResult("https://example-500.org", 500),
+            ]
+        )
+
+        filtered = results.filter_by_status_code(200)
+        self.assertEqual(1, len(filtered))
+        self.assertEqual("https://example-200.org", filtered[0].url)
+        self.assertEqual(200, filtered[0].status_code)
