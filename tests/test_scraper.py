152247cd6db9c6bf1c8e3e9c4a1baf5fed1caf8a
Malachi Soord <inverse.chi@gmail.com>
2025-10-02 23:26:15 +0200

    Add pyright pre-commit

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -8,30 +8,28 @@ from cert_host_scraper import scraper
 TIMEOUT = 2
 
 
+@pytest.mark.enable_socket
 class TestScraper(TestCase):
-    @pytest.mark.enable_socket
-    @vcr.use_cassette("fixtures/vcr/google.com.yaml")
     def test_fetch_urls_clean_true(self):
-        results = scraper.fetch_urls(
-            "google.com", scraper.Options(timeout=2, clean=True)
-        )
-        self.assertEqual(53, len(results))
-        self.assertIn("https://www.google.com", results)
+        with vcr.use_cassette("fixtures/vcr/google.com.yaml"):
+            results = scraper.fetch_urls(
+                "google.com", scraper.Options(timeout=2, clean=True)
+            )
+            self.assertEqual(53, len(results))
+            self.assertIn("https://www.google.com", results)
 
-    @pytest.mark.enable_socket
-    @vcr.use_cassette("fixtures/vcr/google.com.yaml")
     def test_fetch_urls_clean_false(self):
-        results = scraper.fetch_urls(
-            "google.com", scraper.Options(timeout=2, clean=False)
-        )
-        self.assertEqual(60, len(results))
-        self.assertIn("https://*.mail.google.com", results)
+        with vcr.use_cassette("fixtures/vcr/google.com.yaml"):
+            results = scraper.fetch_urls(
+                "google.com", scraper.Options(timeout=2, clean=False)
+            )
+            self.assertEqual(60, len(results))
+            self.assertIn("https://*.mail.google.com", results)
 
-    @pytest.mark.enable_socket
-    @vcr.use_cassette("fixtures/vcr/fetch_site_information_valid.yaml")
     def test_fetch_site_information_valid(self):
-        result = scraper.fetch_site_information("https://example.org", TIMEOUT)
-        self.assertEqual(200, result)
+        with vcr.use_cassette("fixtures/vcr/fetch_site_information_valid.yaml"):
+            result = scraper.fetch_site_information("https://example.org", TIMEOUT)
+            self.assertEqual(200, result)
 
 
 class TestResults(TestCase):
