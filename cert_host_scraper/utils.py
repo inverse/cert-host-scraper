e0d055cbb3485124777cda6963f1aefdce4ec544
Malachi Soord <inverse.chi@gmail.com>
2026-07-25 23:22:12 +0200

    refactor: remove dead code (divide_chunks, search_urls)

--- cert_host_scraper/utils.py
+++ cert_host_scraper/utils.py
@@ -1,13 +1,7 @@
 import re
-from collections.abc import Iterable
 
 
 def strip_url(url: str) -> str:
     url = re.sub(r"https?://", "", url)
     url = re.sub(r"www.", "", url)
     return re.sub(r"/.*", "", url)
-
-
-def divide_chunks(objects: list, size: int) -> Iterable[list[str]]:
-    for i in range(0, len(objects), size):
-        yield objects[i : i + size]
